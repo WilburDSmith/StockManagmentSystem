c54b33b2d97d4a515d2eb5d415ee952c566aefca
Wilbur Smith <220003033@mycput.ac.za>
2021-04-07 09:57:55 +0200

    Setting up the project

---
+++ src/main/java/za/ac/cput/stockmanagmentsystem/StockTestFile.java
@@ -0,0 +1,9 @@
+package za.ac.cput.stockmanagmentsystem;
+
+/**
+ * THIS FILE IS HERE TO ENSURE GIT TRACKS THE DIRECTORY OF THE JAVA PROJECT
+ * PLEASE DELETE THIS FILE ONCE YOU BEGIN CODING
+ */
+public class StockTestFile {
+    
+}
---
+++ src/test/java/za/ac/cput/stockmanagmentsystem/StockTestFileTest.java
@@ -0,0 +1,11 @@
+package za.ac.cput.stockmanagmentsystem;
+
+
+/**
+ * THIS FILE IS HERE TO ENSURE GIT TRACKS THE DIRECTORY OF THE JAVA PROJECT
+ * PLEASE DELETE THIS FILE ONCE YOU BEGIN CODING
+ */
+public class StockTestFileTest {
+    
+    
+}
